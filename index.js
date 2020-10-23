c79a0ed516e7a0e44c9a7ad832d8d0a04fcd0f0c
Termisfa <chipichop12@hotmail.com>
2020-10-23 13:35:46 +0200

    test10 cambiada estructura del if

--- index.js
+++ index.js
@@ -17,22 +17,15 @@ app.post('/', function(req, res) {
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
   var botActivator = 'bot '
-  //if (message.text.toLowerCase().substring(0, botActivator.length) === botActivator) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    //return res.end()
-  //}
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  var result = message.text
+  if (message.text.toLowerCase().substring(0, botActivator.length) === botActivator) {
+    var result = message.text
   result = result.substring(botActivator.length) 
   axios
     .post(
       'https://api.telegram.org/bot1336055457:AAHWh5XS1CkeaObc-JKA6yY2TX9pKHxOj-s/sendMessage',
       {
         chat_id: message.chat.id,
-        text: message.text.toLowerCase().substring(0, botActivator.length)
+        text: result
       }
     )
     .then(response => {
@@ -45,6 +38,8 @@ app.post('/', function(req, res) {
       console.log('Error :', err)
       res.end('Error :' + err)
     })
+  }
+  
 })
 
 // Finally, start our server
