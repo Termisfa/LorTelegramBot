7a7ff5d19c7902a7a28225e3df7f84c7c232757c
Termisfa <chipichop12@hotmail.com>
2020-10-27 22:45:32 +0100

    Avances en merge imagenes

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const mergeImg = require('merge-img')
+
 const fs = require('fs')
 const request = require('request')
 
@@ -9,9 +11,19 @@ const download = (url, path, callback) => {
   })
 }
 
-const url = 'https://dd.b.pvp.net/1_12_0/set3/es_es/img/cards/03MT041.png'
-const path = './image.png'
-
-download(url, path, () => {
-  console.log('✅ Done!')
+download('https://dd.b.pvp.net/1_12_0/set3/es_es/img/cards/03MT041.png', './image.png', () => {
+  console.log('✅ Done1!')
 })
+
+download('https://dd.b.pvp.net/1_12_0/set3/es_es/img/cards/03MT005.png', './image1.png', () => {
+  console.log('✅ Done2!')
+})
+
+mergeImg(['image.png', 'image1.png'])
+.then((img) => {
+  // Save image as file
+  img.write('out.png', () => console.log('done3'));
+});
+
+
+ 
