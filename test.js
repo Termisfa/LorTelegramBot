581e4fc1a919e5410f04209ed0cc59ed650ccc7f
Termisfa <chipichop12@hotmail.com>
2020-10-29 12:24:46 +0100

    Push for test, no changes

--- test.js
+++ test.js
@@ -13,5 +13,5 @@ const url = 'https://dd.b.pvp.net/1_12_0/set3/es_es/img/cards/03MT041.png'
 const path = './image.png'
 
 download(url, path, () => {
-  console.log('✅ Done!')
+  console.log('✅ Done!!!')
 })
